d4ce0d87523afcd82b91085bddceeed30083836d
Mohammad <m.hgzadeh@gmail.com>
2024-08-23 22:29:06 +0200

    fix sessions app installation

--- storefront/settings.py
+++ storefront/settings.py
@@ -24,14 +24,16 @@ SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
@@ -40,7 +42,6 @@ INSTALLED_APPS = [
     'tags',
     'likes'
 ]
-
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
