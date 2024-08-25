4ae2a1a8fc17028e94e921c5445588284f3211a9
Mohammad <m.hgzadeh@gmail.com>
2024-08-25 17:48:47 +0200

    values; in operator; order_by

--- playground/views.py
+++ playground/views.py
@@ -9,5 +9,5 @@ def say_hello(request):
     # products = Product.objects.filter(~Q(inventory__lt=10) | ~Q(inventory__gt=11))
     # products = Product.objects.filter(inventory=F('collection__id'))
     ordered_product_ids = OrderItem.objects.values('product_id').distinct()
-    products = Product.objects.filter(id__in=ordered_product_ids)
+    products = Product.objects.filter(id__in=ordered_product_ids).order_by('title')
     return render(request, template_name='hello.html', context={'name': 'Mosh', 'products': list(products)})
