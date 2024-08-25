b27d9bfbc7938ec11f3c7fa7a4ea78f3f808a698
Mohammad <m.hgzadeh@gmail.com>
2024-08-25 16:35:09 +0200

    add F class

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
-from django.db.models import Q
+# from django.db.models import Q
+from django.db.models import F
 from django.shortcuts import render
 
 from store.models import Product
@@ -6,5 +7,6 @@ from store.models import Product
 
 def say_hello(request):
     # products = Product.objects.all()
-    products = Product.objects.filter(~Q(inventory__lt=10) | ~Q(inventory__gt=11))
+    # products = Product.objects.filter(~Q(inventory__lt=10) | ~Q(inventory__gt=11))
+    products = Product.objects.filter(inventory=F('collection__id'))
     return render(request, template_name='hello.html', context={'name': 'Mosh', 'products': list(products)})
