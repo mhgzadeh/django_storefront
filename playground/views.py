fb9a07abd987757f18cd6162e44f36972d29537d
Mohammad <m.hgzadeh@gmail.com>
2024-08-25 11:30:20 +0200

    update return value

--- playground/views.py
+++ playground/views.py
@@ -5,4 +5,4 @@ from store.models import Product
 
 def say_hello(request):
     query_set = Product.objects.get(pk=1)
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, template_name='hello.html', context={'name': 'Mosh', 'query_set': query_set})
