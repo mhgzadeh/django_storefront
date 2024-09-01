57a99f55f491fdb5fbd8d0f78505730d89bbbb4a
Mohammad <m.hgzadeh@gmail.com>
2024-09-01 10:10:06 +0200

    register django admin; adding computed columns

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Product, Collection, Customer
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'unit_price', 'inventory_status')
+    list_editable = ('unit_price',)
+    list_per_page = 10
+
+    @staticmethod
+    @admin.display(ordering='inventory')
+    def inventory_status(obj):
+        return 'Low' if obj.inventory < 10 else 'Ok'
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'featured_product')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'membership')
+    list_editable = ('membership',)
+    list_per_page = 10
