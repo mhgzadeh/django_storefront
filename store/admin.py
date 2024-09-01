5c4064f9308325c616b620364756174a13de57c0
Mohammad <m.hgzadeh@gmail.com>
2024-09-01 14:39:34 +0200

    adding searchbar and filter; add custom filter

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import register, display
-from django.db.models import Count
+from django.db.models import Count, QuerySet
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
@@ -23,10 +23,24 @@ class CollectionAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(products_count=Count('products')).filter(products_count__gt=0)
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [('low', 'Low')]
+
+    def queryset(self, request, queryset: QuerySet):
+        # 'low is the filter value coming from the frontend'
+        if self.value() == 'low':
+            return queryset.filter(inventory__lt=10)
+
+
 @register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'unit_price', 'inventory_status', 'collection_title')
     list_editable = ('unit_price',)
+    list_filter = ('collection', 'last_update', InventoryFilter)
     list_per_page = 10
     list_select_related = ('collection',)
 
@@ -46,6 +60,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'membership', 'order_count')
     list_editable = ('membership',)
     list_per_page = 10
+    search_fields = ('first_name__istartswith', 'last_name__istartswith')
 
     @staticmethod
     def order_count(obj):
