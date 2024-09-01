43a900286772a8c68d310e20f8f779ca9a4e8ca5
Mohammad <m.hgzadeh@gmail.com>
2024-09-01 12:44:57 +0200

    providing links to other pages

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import register, display
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from store.models import Product, Collection, Customer, Order
 
@@ -11,8 +14,10 @@ class CollectionAdmin(admin.ModelAdmin):
     list_per_page = 10
 
     @staticmethod
+    @display(ordering='products_count')
     def products_count(obj):
-        return obj.products_count
+        url = f"{reverse('admin:store_product_changelist')}?{urlencode({'collection_id': str(obj.id)})}"
+        return format_html(f"<a href='{url}'>{obj.products_count}</a>")
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count('products')).filter(products_count__gt=0)
@@ -38,9 +43,16 @@ class ProductAdmin(admin.ModelAdmin):
 
 @register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'membership')
+    list_display = ('first_name', 'last_name', 'membership', 'order_count')
     list_editable = ('membership',)
     list_per_page = 10
+
+    @staticmethod
+    def order_count(obj):
+        return obj.order_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(order_count=Count('orders'))
 
 
 @admin.register(Order)
