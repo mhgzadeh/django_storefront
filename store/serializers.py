1250101fa38333d499453817759f6136de446b8d
Mohammad <m.hgzadeh@gmail.com>
2024-09-03 23:29:43 +0200

    custom serializer field

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
 from store.models import Product
@@ -6,4 +8,10 @@ from store.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'unit_price')
+        fields = ('id', 'title', 'unit_price', 'price_with_tax')
+
+    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+
+    @staticmethod
+    def get_price_with_tax(obj: Product):
+        return obj.unit_price * Decimal(1.1)
