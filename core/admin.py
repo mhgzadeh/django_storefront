ccc2c7f68a7fef96222e63b97bc2bb32dbbe336b
Mohammad <m.hgzadeh@gmail.com>
2024-09-15 14:24:45 +0200

    add core app

---
+++ core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core.models import User
+
+
+@register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
