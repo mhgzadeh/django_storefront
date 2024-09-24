e0ca9f53e4829c0bc5d933233326f259525396c3
Mohammad <m.hgzadeh@gmail.com>
2024-09-24 19:39:12 +0200

    add new json web token

--- helper/update_serial_sequence.sql
+++ helper/update_serial_sequence.sql
@@ -1,2 +1,5 @@
-SELECT setval(pg_get_serial_sequence('store_product', 'id'), COALESCE(MAX(id), 1), MAX(id) IS NOT NULL)
-FROM store_product;
+SELECT setval(pg_get_serial_sequence('store_order', 'id'), COALESCE(MAX(id), 1), MAX(id) IS NOT NULL)
+FROM store_order;
+
+select setval(pg_get_serial_sequence('store_orderitem', 'id'), coalesce(max(id), 1), max(id) is not null)
+from store_orderitem;
